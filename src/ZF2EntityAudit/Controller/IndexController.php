c16e946c0fb8342670260ac4fa87d17c5118ace6
Tawfek Daghistani <tawfekov@hotmail.com>
2012-11-04 18:02:35 +0200

    creating an empty controller for viewing audits

---
+++ src/ZF2EntityAudit/Controller/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ZF2EntityAudit\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+}
+
