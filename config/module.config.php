41b153664ec915f8b689b158bb199e927cdd8cb9
Tawfek Daghistani <tawfekov@hotmail.com>
2012-11-13 11:56:11 +0200

    started to work on web interface

--- config/module.config.php
+++ config/module.config.php
@@ -1,9 +1,64 @@
 <?php
 
 return array(
+    'router' => array(
+        'routes' => array(
+            // The following is a route to simplify getting started creating
+            // new controllers and actions without needing to create a new
+            // module. Simply drop new controllers in, and you can access them
+            // using the path /application/:controller/:action
+            'audit' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/audit',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'zf2entityaudit\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'ZF2EntityAudit\Controller\Index' => 'ZF2EntityAudit\Controller\IndexController'
+        ),
+    ),
     'service_manager' => array(
         'aliases' => array(
             'default' => 'doctrine.entitymanager.orm_default',
         ),
-    )
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
 );
