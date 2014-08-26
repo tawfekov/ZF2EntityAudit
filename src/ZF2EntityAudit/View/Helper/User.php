4c91966115271e9c09ceec26a113f6331a156d35
Jean Rumeau <rumeau@gmail.com>
2014-08-26 03:01:20 -0400

    Visual adjustments

--- src/ZF2EntityAudit/View/Helper/User.php
+++ src/ZF2EntityAudit/View/Helper/User.php
@@ -26,13 +26,13 @@ class User extends AbstractHelper
         $html = '<div class="row">';
         $html .= '<div class="col-lg-8">';
         if ($user->getDisplayName()) {
-            $html .= "<p>DisplayName : {$this->getView()->escapeHtml($user->getDisplayName())}<br/>";
+            $html .= "<p><strong>DisplayName</strong>: {$this->getView()->escapeHtml($user->getDisplayName())}<br/>";
         }
         if ($user->getUserName()) {
-            $html .= "UserName : {$this->getView()->escapeHtml($user->getUserName())}<br/>";
+            $html .= "<strong>UserName</strong>: {$this->getView()->escapeHtml($user->getUserName())}<br/>";
         }
         if ($user->getEmail()) {
-            $html .= "Email Address : {$this->getView()->escapeHtml($user->getEmail())}</p>";
+            $html .= "<strong>Email Address</strong>: {$this->getView()->escapeHtml($user->getEmail())}</p>";
         }
         $html .= '<span class="label label-info">15 insert</span> ';
         $html .= '<span class="label label-warning">8 updates</span> ';
@@ -48,7 +48,7 @@ class User extends AbstractHelper
     public function Gravatar($email)
     {
         $options = array(
-            'img_size' => 96,
+            'img_size' => 90,
             'default_img' => 'mm',
             'rating' => 'g',
             'secure' => false,
